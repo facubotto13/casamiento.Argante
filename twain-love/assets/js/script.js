55a4098a21d7ae86bb1044b47b05a132ad82803e
facubotto13 <151096391+facubotto13@users.noreply.github.com>
2025-05-27 23:23:44 -0300

    commit

--- twain-love/assets/js/script.js
+++ twain-love/assets/js/script.js
@@ -423,12 +423,12 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/12/31', function(event) {
+        $('#clock').countdown('2025/06/21', function(event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
-            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
-            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+            + '<div class="box"><div>%D</div> <span>Días</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Horas</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Minutos</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Segundos</span> </div>'));
         });
     }
 
